e852df9f1320d06d02e38ad42fc236a9fb2018dc
Ryan Macnak <rmacnak@gmail.com>
2013-08-29 17:52:12 -0700

    Add tests for numeric and boolean literals.

--- testDartGrammar.dart
+++ testDartGrammar.dart
@@ -10,12 +10,12 @@ library test_dart_grammar;
 
 import 'CombinatorialParsing.dart';
 import 'dart_runnable_grammar.dart' as dartRunnableGrammar;
-import 'dartGrammar.dart' as dartExecutableGrammar;
+import 'dart_executable_grammar.dart' as dartExecutableGrammar;
 
 accept(production, input) {
   try {
   	production.parse(input);
-  	print("PASS: Accepted <$input>");
+  	// print("PASS: Accepted <$input>");
   } catch(e) {
     print("FAIL: Should have accepted <$input>");  	
   }
@@ -23,16 +23,14 @@ accept(production, input) {
 
 reject(production, input) {
   try {
-  	print(production.parse(input));
+  	production.parse(input);
   	throw "FAIL: Should have rejected <$input>";
   } catch(e) {
-  	print("PASS: Rejected <$input>");
+  	// print("PASS: Rejected <$input>");
   }
 }
 
 test(grammar) {
-  reject(grammar.identifier, '9');
-  reject(grammar.identifier, '');
   accept(grammar.identifier, 'foo');
   accept(grammar.identifier, 'bar9');
   accept(grammar.identifier, 'dollar\$');
@@ -41,11 +39,56 @@ test(grammar) {
   accept(grammar.identifier, '_dollar\$');
   accept(grammar.identifier, '\$');
   accept(grammar.identifier, ' leadingSpace');
+  reject(grammar.identifier, '9');
+  reject(grammar.identifier, '3foo');
+  reject(grammar.identifier, '');
+
+  accept(grammar.numericLiteral, '0');
+  accept(grammar.numericLiteral, '1984');
+  accept(grammar.numericLiteral, ' 1984');
+  accept(grammar.numericLiteral, '-1984');
+  accept(grammar.numericLiteral, '0xCAFE');
+  accept(grammar.numericLiteral, '0XCAFE');
+  accept(grammar.numericLiteral, '0xcafe');
+  accept(grammar.numericLiteral, '0Xcafe');
+  accept(grammar.numericLiteral, '0xCaFe');
+  accept(grammar.numericLiteral, '0XCaFe');
+  accept(grammar.numericLiteral, '3e4');
+  accept(grammar.numericLiteral, '3e-4');
+  accept(grammar.numericLiteral, '-3e4');
+  accept(grammar.numericLiteral, '-3e-4');
+  accept(grammar.numericLiteral, '3E4');
+  accept(grammar.numericLiteral, '3E-4');
+  accept(grammar.numericLiteral, '-3E4');
+  accept(grammar.numericLiteral, '-3E-4');
+  reject(grammar.numericLiteral, '-3e--4');
+  reject(grammar.numericLiteral, '-0xCAFE');
+  reject(grammar.numericLiteral, '-0XCAFE');
+  reject(grammar.numericLiteral, 'CAFE');
+  reject(grammar.numericLiteral, '0xGHIJ');
+  reject(grammar.numericLiteral, '-');
+  reject(grammar.numericLiteral, '');
+
+  accept(grammar.booleanLiteral, 'true');
+  accept(grammar.booleanLiteral, 'false');
+  accept(grammar.booleanLiteral, ' true');
+  accept(grammar.booleanLiteral, ' false');
+  reject(grammar.booleanLiteral, '9');
+  reject(grammar.booleanLiteral, '"foo"');
+  reject(grammar.booleanLiteral, "'foo'");
+  reject(grammar.booleanLiteral, 'TRUE');
+  reject(grammar.booleanLiteral, 'FALSE');
+  reject(grammar.booleanLiteral, 'null');
+  reject(grammar.booleanLiteral, '0xCAFE');
+
+  // Do we want to use the consuming or non-consuming variant of #not?
+  //accept(grammar.stringLiteral, '"foo"');
+  //accept(grammar.stringLiteral, "'foo'");
 }
 
 main() {
-  print('runnable');
+  print('Runnable:');
   test(new dartRunnableGrammar.DartGrammar());
-  print('executable');
+  print('Executable:');
   test(new dartExecutableGrammar.DartGrammar());
 }
